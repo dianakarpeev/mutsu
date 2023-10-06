66c1c57d3960d297947233e9e2f13bb7a73b2345
ClockWorkElementals <hynesaiden02@gmail.com>
2023-10-06 18:43:06 -0400

    Mel's content pushed to branch, includes ShoppingList.kt and Food.kt, along with 3 drawable pictures -M

---
+++ app/src/main/java/com/example/emptyactivity/Food.kt
@@ -0,0 +1,30 @@
+package com.example.emptyactivity
+
+data class Ingredient(val name: String, var portions : Int)
+data class Meal(val name: String, val ingredients: List<Ingredient>)
+
+
+//Hamburger
+val bread = Ingredient("Bread", 2)
+val beefPatty = Ingredient("Beef Patty", 1)
+val tomato = Ingredient("Tomato", 1)
+val onion = Ingredient("Onion", 1)
+val cabbage = Ingredient("Cabbage", 1)
+
+//Beef Stew
+val beef = Ingredient("1/4lb of Beef", 1)
+val carrot = Ingredient("Carrot", 2)
+val potato = Ingredient("Potato", 2)
+
+//Bacon and Eggs
+val bacon = Ingredient("Bacon", 4)
+val eggs = Ingredient("Egg", 2)
+
+val hamburgerIngredient = listOf(bread, beefPatty, tomato, onion, cabbage)
+val beefStewIngredient = listOf(beef, carrot, potato)
+val baconAndEggsIngredient = listOf(bread, bacon, eggs)
+
+
+val hamburgerMeal = Meal("Hamburger", hamburgerIngredient)
+val beefStewMeal = Meal("Beef Stew", beefStewIngredient)
+val baconAndEggsMeal = Meal("Bacon and Eggs", baconAndEggsIngredient)

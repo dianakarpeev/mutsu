8911002c363c37096e25f2f75fb6fd2c8d31ef5c
Breanna <bdeforest01@gmail.com>
2023-11-20 17:08:59 -0500

    add screen destinations in MustuDestination
    created an interface and objects to represent the screens we are using so far.

---
+++ app/src/main/java/com/example/emptyactivity/MutsuDestination.kt
@@ -0,0 +1,46 @@
+package com.example.emptyactivity
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+
+interface MutsuDestination {
+    val icon: ImageVector
+    val route: String
+    val screen: @Composable () -> Unit
+}
+
+object Home : MutsuDestination {
+    override val icon = Icons.Filled.Home
+    override val route = "home"
+    override val screen : @Composable () -> Unit = { MutsuHomeScreen() }
+}
+
+object GroceryList : MutsuDestination {
+    override val icon = Icons.Filled.Star
+    override val route = "grocery-list"
+    override val screen : @Composable () -> Unit = { IngredientsScreen() }
+}
+
+object MealPlan : MutsuDestination {
+    override val icon = Icons.Filled.ShoppingCart
+    override val route = "meal-plan"
+    override val screen : @Composable () -> Unit = { foodCounter() }
+}
+
+object RecipeList : MutsuDestination {
+    override val icon = Icons.Filled.Star
+    override val route = "recipe-list"
+    override val screen : @Composable () -> Unit = { RecipeListScreen() }
+}
+
+object AboutUs : MutsuDestination {
+    override val icon = Icons.Filled.Star
+    override val route = "about-us"
+    override val screen : @Composable () -> Unit = { AboutUsScreen() }
+}
+
+val mustuTabRowScreens = listOf(Home, RecipeList, MealPlan, GroceryList)

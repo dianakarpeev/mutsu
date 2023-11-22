bd2d5fd912fb9956b823374f6d770a10ab8cdf23
breannadefo <98484910+breannadefo@users.noreply.github.com>
2023-11-22 17:50:07 -0500

    Fix navigation (#52)
    * add screen destinations in MustuDestination
    
    created an interface and objects to represent the screens we are using so far.
    
    * set up NavController
    
    to make sure the navigation is done properly, we needed to set up NavController.
    
    changes made:
    - added navigation dependency to the project.
    - created a new MutsuApp composable and added a NavHost to it.
    - created a new folder for navigation files.
    
    * implemented navigation using NavController
    
    set up a helper function to navigate to the different screens. These functions are now called in the onClick functions for each navbar element.
    
    also updated recepe screen object name to be 'Recipes' instead of 'RecipeList'.
    
    * access different screens from main app
    
    moved the code that calls a specific screen from the MutsuDestination objects to the composables in the MutsuApp composable.
    
    * access about us screen from home page
    
    * remove old navigation code

--- app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -10,9 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.ShoppingCart
@@ -30,13 +27,24 @@ import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSiz
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.ui.Modifier
 import com.example.emptyactivity.ui.theme.EmptyActivityTheme
-import androidx.compose.material3.windowsizeclass.WindowSizeClass
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.emptyactivity.navigation.AboutUs
+import com.example.emptyactivity.navigation.GroceryList
+import com.example.emptyactivity.navigation.Home
+import com.example.emptyactivity.navigation.MealPlan
+import com.example.emptyactivity.navigation.MutsuDestination
+import com.example.emptyactivity.navigation.Recipes
+import com.example.emptyactivity.navigation.mustuTabRowScreens
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class,
@@ -44,55 +52,85 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EmptyActivityTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    var currentScreen by rememberSaveable { mutableStateOf("Home") }
-                    Scaffold(
-                        topBar = { TopAppBar(title = { Text("MyApp")})},
-                        bottomBar = {
-                            BottomAppBar {
-                                Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
-                                    IconButton(onClick = {currentScreen = "Home"} ) {
-                                        Icon(Icons.Filled.Home, contentDescription = "Home")
-                                    }
-                                    IconButton(onClick = { currentScreen = "Ingredients"} ) {
-                                        Icon(Icons.Filled.Star, contentDescription = "Menu")
-                                    }
-                                    IconButton(onClick = { currentScreen = "Shopping List"} ) {
-                                        Icon(Icons.Filled.ShoppingCart, contentDescription = "Add")
-                                    }
-                                    IconButton(onClick = { currentScreen = "Recipe List" }) {
-                                        Icon(Icons.Filled.Menu, contentDescription = "Call")
-                                    }
+            MutsuApp()
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class,
+        ExperimentalMaterial3Api::class)
+    @Composable
+    fun MutsuApp(){
+        EmptyActivityTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                val navController = rememberNavController()
+
+                val currentBackStack by navController.currentBackStackEntryAsState()
+                val currentDestination = currentBackStack?.destination
+
+                val windowSizeClass = calculateWindowSizeClass(this)
+
+                Scaffold(
+                    topBar = { TopAppBar(title = { Text("MyApp")})},
+                    bottomBar = {
+                        BottomAppBar {
+                            Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+                                IconButton(
+                                    onClick = { navController.navigateSingleTopTo(Home.route) }
+                                ){
+                                    Icon(Home.icon, contentDescription = "Home")
+                                }
+                                IconButton(
+                                    onClick = { navController.navigateSingleTopTo(GroceryList.route) }
+                                ){
+                                    Icon(GroceryList.icon, contentDescription = "Grocery List")
+                                }
+                                IconButton(
+                                    onClick = { navController.navigateSingleTopTo(MealPlan.route) }
+                                ){
+                                    Icon(MealPlan.icon, contentDescription = "Meal Plan")
+                                }
+                                IconButton(
+                                    onClick = { navController.navigateSingleTopTo(Recipes.route) }
+                                ) {
+                                    Icon(Recipes.icon, contentDescription = "Recipes")
                                 }
                             }
                         }
-                    ) {
-
-                        //determines which navigation bar to use based on the current window size
-                        val windowSizeClass = calculateWindowSizeClass(this)
-
-                        Column(modifier = Modifier.padding(paddingValues = it)) {
-                            when (currentScreen) {
-                                "Home" -> MutsuHomeScreen({ currentScreen = "About Us" })
-                                "About Us" -> AboutUsScreen()
-                                "Ingredients" -> IngredientsScreen()
-                                "Shopping List" -> foodCounter()
-                                "Recipe List" -> RecipeListScreen(windowSizeClass)
-                            }
-                        }
-
-
                     }
-
-
-                    //here is where we call the different screens
+                ){innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = Home.route,
+                        modifier = Modifier.padding(innerPadding)
+                    ){
+                        composable(route = Home.route){
+                            MutsuHomeScreen(
+                                goToAboutUs = {
+                                    navController.navigateSingleTopTo(AboutUs.route)
+                                }
+                            )
+                        }
+                        composable(route = MealPlan.route){
+                            foodCounter()
+                        }
+                        composable(route = Recipes.route){
+                            RecipeListScreen(windowSizeClass)
+                        }
+                        composable(route = GroceryList.route){
+                            IngredientsScreen()
+                        }
+                        composable(route = AboutUs.route){
+                            AboutUsScreen()
+                        }
+                    }
                 }
             }
         }
     }
 }
+
+fun NavHostController.navigateSingleTopTo(route: String) =
+    this.navigate(route) { launchSingleTop = true }

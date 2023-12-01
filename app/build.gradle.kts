78ce8667bf3a52ff48bc0a286047b88d61790dd9
Breanna <bdeforest01@gmail.com>
2023-11-30 21:40:41 -0500

    fixed dependency for firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,5 +77,6 @@ dependencies {
 
     //firebase
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
-    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
+    //implementation("com.firebaseui:firebase-ui-auth:7.2.0")
+    implementation("com.google.firebase:firebase-auth-ktx")
 }

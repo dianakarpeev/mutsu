4380158d0f29de93840a84e105ce06bb4ec728a0
Breanna <bdeforest01@gmail.com>
2023-12-01 04:33:38 -0500

    remove old and unused code from login page

--- app/src/main/java/com/example/mutsu/loginRegistration/LoginRegister.kt
+++ app/src/main/java/com/example/mutsu/loginRegistration/LoginRegister.kt
@@ -33,16 +33,6 @@ fun LoginRegisterScreen(
         authViewModel: AuthViewModel = viewModel(factory= AuthViewModelFactory()),
         modifier: Modifier = Modifier)
 {
-    /*
-    var tempUserData = mutableListOf<User> ()
-    tempUserData.add(User("hellokitty", "kitty1234", "katrina"))
-    tempUserData.add(User("dance4life", "hip82hop", "devin"))
-    tempUserData.add(User("maarrrllliie", "pASSW0Rd", "marls <3"))
-    tempUserData.add(User("__ice__tea__", "nine8seven", "trin"))
-    tempUserData.add(User("em.meme.em", "3l3vator", "E M E L I O"))
-
-
-*/
     var optionShown by rememberSaveable { mutableStateOf("") }
     var currentUser = authViewModel.currentUser().collectAsState()
 
@@ -99,7 +89,7 @@ fun LoginRegisterScreen(
                     Button(onClick = {
                         var emailMatches = goodEmailRegex.matcher(email)
 
-                        if(email == "" || password == "" || email == " " || password == " "){
+                        if(email == null || password == null || email == "" || password == "" || email == " " || password == " "){
                             message = "Both the email and password fields have to be filled out to register."
                         }
                         else if (!emailMatches.matches()) {
@@ -116,8 +106,6 @@ fun LoginRegisterScreen(
                         Text("Login here")
                     }
                 }
-
-                //Login(tempUserData, modifier.padding(15.dp))
             }
             else if (optionShown == "Register"){
                 var email by rememberSaveable { mutableStateOf("") }
@@ -161,14 +149,10 @@ fun LoginRegisterScreen(
                         Text("Register here")
                     }
                 }
-
-                //Register(tempUserData, modifier.padding(15.dp))
             }
         }
     }
     else {
-        Text("${currentUser.value}")
-
         var showPopUp by rememberSaveable { mutableStateOf(false) }
 
         Column(Modifier.padding(15.dp)){
@@ -229,106 +213,4 @@ fun ConfirmDeleteAccount(confirm: () -> Unit, dismiss: () -> Unit = {}){
             }
         }
     )
-}
-
-@Composable
-fun Login(users: MutableList<User>, modifier: Modifier = Modifier){
-    var username by rememberSaveable { mutableStateOf("") }
-    var password by rememberSaveable { mutableStateOf("") }
-    var message by rememberSaveable { mutableStateOf("") }
-
-    Column(modifier.padding(15.dp)){
-        TextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Enter your username: ")}
-        )
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Enter your password: ")}
-        )
-
-        Button(onClick = {
-            message = ""
-            if (username == "" || password == ""){
-                message = "You must enter a username and a password to login."
-            }
-            for (u in users){
-                if (u.username == username){
-                    if (u.password == password){
-                        message = "Welcome $username! You are now logged in."
-                        break
-                    }
-                    else {
-                        message = "Incorrect password."
-                        break
-                    }
-                }
-            }
-            if (message == ""){
-                message = "Invalid username."
-            }
-        }){
-            Text("Let's go!")
-        }
-        Text("$message")
-    }
-}
-
-@Composable
-fun Register(users: MutableList<User>, modifier: Modifier = Modifier){
-    var username by rememberSaveable { mutableStateOf("") }
-    var password by rememberSaveable { mutableStateOf("") }
-    var passwordVerification by rememberSaveable { mutableStateOf("") }
-    var name by rememberSaveable { mutableStateOf("") }
-    var message by rememberSaveable { mutableStateOf("") }
-
-    Column(modifier.padding(15.dp)){
-        TextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Enter a new username: ")}
-        )
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Enter a new password: ")}
-        )
-        TextField(
-            value = passwordVerification,
-            onValueChange = { passwordVerification = it },
-            label = { Text("Reenter your new password: ")}
-        )
-        TextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Enter a display name: ")}
-        )
-
-        Button(onClick = {
-            message = ""
-            if (username == "" || password == "" || passwordVerification == "" || name == ""){
-                message = "The username and password fields cannot be empty to be able to create an account."
-            }
-            for (u in users){
-                if (u.username == username){
-                    message = "This username is already taken. Try another one."
-                    break
-                }
-            }
-            if (message == ""){
-                if (password == passwordVerification){
-                    message = "Welcome $name! An account has been created with the username: $username"
-                    //add user to list of existing users here
-                }
-                else {
-                    message = "Both passwords must be the same to create an account."
-                }
-            }
-        }){
-            Text("Let's go!")
-        }
-        Text("$message")
-    }
 }

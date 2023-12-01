3d91fbee56e7a8dd3441e7139f15493446f17ce8
Diana Karpeev <2059788@johnabbottcollege.net>
2023-12-01 14:26:10 -0500

    fix: tweak login register buttons

--- app/src/main/java/com/example/mutsu/loginRegistration/LoginRegister.kt
+++ app/src/main/java/com/example/mutsu/loginRegistration/LoginRegister.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -22,6 +23,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import java.util.regex.Pattern
@@ -42,8 +44,6 @@ fun LoginRegisterScreen(
 
     if (currentUser.value == null){
         Column(Modifier.padding(15.dp), horizontalAlignment = Alignment.CenterHorizontally){
-            Text("Welcome. Click log in to access the entire app. If you don't already have an account, you can create one by clicking register.")
-
             Row(modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround){
                 Button(onClick = {
                     if (optionShown != "Login") {
@@ -53,7 +53,11 @@ fun LoginRegisterScreen(
                         optionShown = ""
                     }
                 }) {
-                    Text("Log in")
+                    Text(
+                        text ="Log in",
+                        fontWeight = FontWeight.Medium,
+                        style = MaterialTheme.typography.titleMedium
+                    )
                 }
 
                 Button(onClick = {
@@ -64,7 +68,11 @@ fun LoginRegisterScreen(
                         optionShown = ""
                     }
                 }) {
-                    Text("Register")
+                    Text(
+                        text = "Register",
+                        fontWeight = FontWeight.Medium,
+                        style = MaterialTheme.typography.titleMedium
+                    )
                 }
             }
             if (optionShown == "Login"){

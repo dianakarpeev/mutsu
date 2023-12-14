e6cd112cad054db86f0eb1068ad70b7f1d5e9f36
Diana Karpeev <2059788@johnabbottcollege.net>
2023-12-13 19:10:22 -0500

    feat: add toggle text under form

--- app/src/main/java/com/example/mutsu/loginRegistration/LoginRegister.kt
+++ app/src/main/java/com/example/mutsu/loginRegistration/LoginRegister.kt
@@ -1,17 +1,13 @@
 package com.example.mutsu.loginRegistration
 
-import android.graphics.drawable.Icon
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -24,10 +20,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlinx.coroutines.flow.StateFlow
 import java.util.regex.Pattern
 
 data class User (var username : String, var password : String, var name : String )
@@ -36,35 +32,33 @@ data class User (var username : String, var password : String, var name : String
 fun LoginRegisterScreen(
     authViewModel: AuthViewModel = viewModel(factory= AuthViewModelFactory())
 ) {
-    var optionShown by rememberSaveable { mutableStateOf("") }
-    var currentUser = authViewModel.currentUser().collectAsState()
+    val defaultOption = "Register"
+    val currentUser = authViewModel.currentUser().collectAsState()
 
     if (currentUser.value == null){
         Column(Modifier.padding(15.dp), horizontalAlignment = Alignment.CenterHorizontally){
             AuthenticationForm(
                 authViewModel = authViewModel,
                 currentUser = currentUser,
-                formType = optionShown
+                initialFormType = defaultOption
             )
         }
     }
     else {
-        SignedInScreen(modifier = Modifier)
+        SignedInScreen(authViewModel)
     }
-}
-
-fun isNullOrEmpty(input: String?): Boolean {
-    return input == null || input.isEmpty()
 }
 
 @Composable
 fun AuthenticationForm(
     authViewModel: AuthViewModel,
     currentUser: State<Users?>,
-    formType: String,
+    initialFormType: String,
     modifier: Modifier = Modifier
 ) {
+
     val goodEmailRegex = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
+    var formType by rememberSaveable { mutableStateOf(initialFormType) }
     var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
     var message by rememberSaveable { mutableStateOf("") }
@@ -81,12 +75,12 @@ fun AuthenticationForm(
             label = { Text("Enter your password: ") }
         )
 
-        Text("$message")
+        Text(message)
 
         Button(onClick = {
             val emailMatches = goodEmailRegex.matcher(email)
 
-            if (isNullOrEmpty(email) || isNullOrEmpty(password))
+            if (email.isEmpty() || password.isEmpty())
                 message = "Both the email and password fields have to be filled out."
             else if (!emailMatches.matches())
                 message = "That is not a valid email address. Please try again."
@@ -109,13 +103,29 @@ fun AuthenticationForm(
                 }
             }
         }) {
-            Text(if (formType == "Login") "Login here" else "Register here")
+            Text(if (formType == "Login") "Sign in" else "Sign up")
         }
+
+        val toggleText = if (formType == "Login") "Don't have an account? Create one" else "Already have an account? Log in"
+
+        Text(
+            text = toggleText,
+            modifier = Modifier.clickable {
+                formType = if (formType == "Login") "Register" else "Login"
+                email = ""
+                password = ""
+                message = ""
+            },
+            color = Color.Blue,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
 @Composable
-fun SignedInScreen(modifier: Modifier){
+fun SignedInScreen(
+    authViewModel: AuthViewModel
+){
     var showPopUp by rememberSaveable { mutableStateOf(false) }
 
     Column(Modifier.padding(15.dp)){

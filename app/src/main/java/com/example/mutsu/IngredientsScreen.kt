6922f5d0edac7272c51c48113265e6294415bcd2
Breanna <bdeforest01@gmail.com>
2023-12-13 21:54:40 -0500

    fix ui of grocery list confirmation popup
    changed the ui so the popup is more defined and easier to understand

--- app/src/main/java/com/example/mutsu/IngredientsScreen.kt
+++ app/src/main/java/com/example/mutsu/IngredientsScreen.kt
@@ -1,7 +1,9 @@
 package com.example.mutsu
 
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -33,6 +35,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -248,8 +251,12 @@ fun ListConfirmationPopUp(
                 onClick = {
                     dismiss()
                     confirm()
-                }
-            ) {
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.background
+                ),
+           ) {
                 Text("Confirm")
             }
         },
@@ -257,10 +264,16 @@ fun ListConfirmationPopUp(
             TextButton(
                 onClick = {
                     dismiss()
-                }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    contentColor = MaterialTheme.colorScheme.background
+                ),
             ) {
                 Text("Dismiss")
             }
-        }
+        },
+        modifier = modifier
+            .border(BorderStroke(2.dp, MaterialTheme.colorScheme.onPrimary), MaterialTheme.shapes.extraLarge)
     )
 }

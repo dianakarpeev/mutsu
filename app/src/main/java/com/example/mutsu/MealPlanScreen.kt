1d6df08fd12c732f8fd4d0069d4dda19b482e195
Breanna <bdeforest01@gmail.com>
2023-12-15 03:13:55 -0500

    remove unused code from MealPlanScreen
    fixed some spacing as well as removed old code

--- app/src/main/java/com/example/mutsu/MealPlanScreen.kt
+++ app/src/main/java/com/example/mutsu/MealPlanScreen.kt
@@ -42,216 +42,8 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 
-/*
-@Composable
-fun ShoppingList(){
-    Header()
-}
-
-@Composable
-fun Header(){
-    Row(){
-        Text(
-            text = "Plan your meals!",
-            style = MaterialTheme.typography.titleMedium,
-        )
-        Icon(
-            imageVector = Icons.Default.DateRange,
-            contentDescription = null
-        )
-        Spacer(modifier = Modifier.width(10.dp))
-        Icon(
-            painter = painterResource(id = R.drawable.receipt),
-            contentDescription = null
-        )
-    }
-}
-
-@Composable
-fun RecipeCard(
-    recipeName: String,
-    count: Int,
-    increase: () -> Unit,
-    decrease: () -> Unit
-){
-    Card {
-        Column(){
-            Text(recipeName)
-            Row(){
-                IconButton(
-                    onClick = increase,
-                    colors = IconButtonDefaults.iconButtonColors(
-                        containerColor = MaterialTheme.colorScheme.primary,
-                        contentColor = MaterialTheme.colorScheme.background
-                    ),
-                    modifier = Modifier.padding(
-                        start = 0.dp,
-                        top = 0.dp,
-                        end = 6.dp,
-                        bottom = 0.dp
-                    )
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Add,
-                        contentDescription = "Add one $recipeName"
-                    )
-                }
-
-                Text(text = count.toString())
-
-                IconButton(
-                    onClick = decrease,
-                    colors = IconButtonDefaults.iconButtonColors(
-                        containerColor = MaterialTheme.colorScheme.secondary,
-                        contentColor = MaterialTheme.colorScheme.background
-                    ),
-                    modifier = Modifier.padding(
-                        start = 0.dp,
-                        top = 0.dp,
-                        end = 6.dp,
-                        bottom = 0.dp
-                    )
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.remove_icon),
-                        contentDescription = "Remove one $recipeName"
-                    )
-                }
-            }
-        }
-    }
-}
-*/
-
-@Composable
-fun Hamburger(count: Int, increase: () -> Unit, decrease: () -> Unit) {
-    val hamburgerImage = painterResource(id = R.drawable.hamburger)
-
-
-    Row(modifier = Modifier
-        .fillMaxWidth(),
-    ) {
-        Image(modifier = Modifier
-            .size(48.dp)
-            .clip(CircleShape),
-            painter = hamburgerImage,
-            contentDescription = "A pixelated hamburger.")
-        Text(modifier = Modifier
-            .padding(start = 8.dp, top = 12.dp),
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Bold,
-            text = "HAMBURGER")
-        Button(
-            onClick = increase,) {
-            Icon(Icons.Filled.KeyboardArrowUp, contentDescription = "Add Meal")
-        }
-        Button(
-            onClick = decrease) {
-            Icon(Icons.Filled.KeyboardArrowDown, "Subtract Meal")
-        }
-        Text(modifier = Modifier
-            .padding(start = 8.dp, top = 12.dp),
-            fontSize = 18.sp,
-            text = "$count")
-    }
-}
-
-
-@Composable
-fun BeefStew(count: Int, increase: () -> Unit, decrease: () -> Unit) {
-    val beefStewImage = painterResource(id = R.drawable.beefstew)
-
-
-    Row(modifier = Modifier
-        .fillMaxWidth(),
-    ) {
-        Image(modifier = Modifier
-            .size(48.dp)
-            .clip(CircleShape),
-            painter = beefStewImage,
-            contentDescription = "A pixelated image of beef stew.")
-        Text(modifier = Modifier
-            .padding(start = 8.dp, top = 12.dp),
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Bold,
-            text = "BEEF STEW")
-        Button(
-            onClick = increase,) {
-            Icon(Icons.Filled.KeyboardArrowUp, contentDescription = "Add Meal")
-        }
-        Button(onClick = decrease) {
-            Icon(Icons.Filled.KeyboardArrowDown, "Subtract Meal")
-        }
-        Text(modifier = Modifier
-            .padding(start = 8.dp, top = 12.dp),
-            fontSize = 18.sp,
-            text = "$count")
-    }
-}
-
-
-@Composable
-fun BaconAndEggs(count: Int, increase: () -> Unit, decrease: () -> Unit) {
-    val baconAndEggsImage = painterResource(id = R.drawable.baconandeggs)
-
-    Row(modifier = Modifier
-        .fillMaxWidth(),
-    ) {
-        Image(modifier = Modifier
-            .size(48.dp)
-            .clip(CircleShape),
-            painter = baconAndEggsImage,
-            contentDescription = "A pixelated image of bacon and eggs.")
-        Text(modifier = Modifier
-            .padding(start = 8.dp, top = 12.dp),
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Bold,
-            text = "BACON AND EGGS")
-        Button(
-            onClick = increase,) {
-            Icon(Icons.Filled.KeyboardArrowUp, contentDescription = "Add Meal")
-        }
-        Button(onClick = decrease) {
-            Icon(Icons.Filled.KeyboardArrowDown, "Subtract Meal")
-        }
-        Text(modifier = Modifier
-            .padding(start = 8.dp, top = 12.dp),
-            fontSize = 18.sp,
-            text = "$count")
-    }
-}
-
-
-
-
-//
 @Composable
 fun MealPlanScreen(mealsViewModel: MealsViewModel,modifier: Modifier = Modifier) {
-    /*
-    var hamburgerCount by rememberSaveable {mutableStateOf(0)}
-    var beefStewCount by rememberSaveable {mutableStateOf(0)}
-    var baconAndEggsCount by rememberSaveable {mutableStateOf(0)}
-
-    val hamburgerDecrease = {if(hamburgerCount > 0) hamburgerCount--}
-    val beefStewDecrease = {if(beefStewCount > 0) beefStewCount--}
-    val baconAndEggsDecrease = {if(baconAndEggsCount> 0) baconAndEggsCount--}
-
-    var allIngredients by rememberSaveable {mutableStateOf(mutableListOf<Ingredient>())}
-
-    Column {
-        Hamburger(hamburgerCount, increase = {hamburgerCount++}, decrease = hamburgerDecrease )
-        BeefStew(beefStewCount, increase = {beefStewCount++}, decrease = beefStewDecrease)
-        BaconAndEggs(baconAndEggsCount, increase = {baconAndEggsCount++}, decrease = baconAndEggsDecrease)
-        Button(onClick = {allIngredients = generateList(hamburgerCount, beefStewCount, baconAndEggsCount,
-            hamburgerDecrease, beefStewDecrease, baconAndEggsDecrease)}) {
-            Text(text = "Generate Ingredients")
-        }
-
-        displayIngredients(allIngredients)
-
-    }
-     */
-
     val meals by mealsViewModel.meals.collectAsStateWithLifecycle()
 
     val increase: (Int) -> Unit = {
@@ -265,16 +57,12 @@ fun MealPlanScreen(mealsViewModel: MealsViewModel,modifier: Modifier = Modifier)
         .verticalScroll(rememberScrollState())
         .padding(16.dp)
     ){
-        //ShowAllMeals(modifier)
         ShowAllMeals(meals, increase, decrease, modifier)
     }
-
-
 }
 
 data class Meals(var recipe: Recipe, var quantity: Int)
 
-//
 @Composable
 fun ShowAllMeals(
     meals: List<Meals>,
@@ -283,8 +71,6 @@ fun ShowAllMeals(
     modifier: Modifier = Modifier){
     Column(modifier = modifier,){
         meals.forEachIndexed { index, it ->
-            //Text(text = "${meals.size}")
-
             MealBox(
                 mealName = it.recipe.name,
                 mealQuantity = it.quantity,
@@ -292,9 +78,7 @@ fun ShowAllMeals(
                 isRemoved = { if (it.quantity > 0) decreaseQuantity(index) },
                 modifier = modifier
             )
-
         }
-        //Text("please?")
     }
 }
 
@@ -378,56 +162,5 @@ fun MealBox(
                 }
             }
         }
-    }
-}
-
-
-
-fun generateList(hamburgers : Int, beefStews : Int, baconAndEggs : Int,
-                 hamburgerDecrease : () -> Unit, beefstewDecrease : () -> Unit, baconAndEggsDecrease: () -> Unit): MutableList<Ingredient> {
-
-    val meals = mutableListOf<Meal>()
-    val ingredients = mutableListOf<Ingredient>()
-
-    for(i in 1..hamburgers) {
-        meals.add(hamburgerMeal)
-        hamburgerDecrease()
-    }
-    for (i in 1..beefStews) {
-        meals.add(beefStewMeal)
-        beefstewDecrease()
-    }
-    for (i in 1..baconAndEggs) {
-        meals.add(baconAndEggsMeal)
-        baconAndEggsDecrease()
-    }
-
-    for((i, currentMeal) in meals.withIndex()) {
-        val currentMealIngredients = currentMeal.ingredients
-        for((j, currentIngredient) in currentMealIngredients.withIndex()) {
-            ingredients.add(currentIngredient)
-        }
-    }
-
-    return ingredients
-}
-
-@Composable
-fun displayIngredients(ingredientList : List<Ingredient>) {
-
-    //Mutable Map implementation aided by Github Copilot
-    val ingredientMap = mutableMapOf<String, Int>()
-
-    for((i, currentIngredient) in ingredientList.withIndex()) {
-        if(ingredientMap.containsKey(currentIngredient.name)) {
-            ingredientMap[currentIngredient.name] = ingredientMap[currentIngredient.name]!! + currentIngredient.portions
-        }
-        else {
-            ingredientMap[currentIngredient.name] = currentIngredient.portions
-        }
-    }
-
-    for((i, currentIngredient) in ingredientMap) {
-        Text(text = "$i : $currentIngredient")
     }
 }

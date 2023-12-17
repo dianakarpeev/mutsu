8c799dbd708f59eab4b62cacae8f97b6585cf8b0
Diana Karpeev <2059788@johnabbottcollege.net>
2023-12-17 15:56:24 -0500

    implement bottom bar/navigation rail

--- app/src/main/java/com/example/mutsu/MainActivity.kt
+++ app/src/main/java/com/example/mutsu/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.mutsu
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -17,15 +21,19 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -36,11 +44,11 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.mutsu.loginRegistration.AuthViewModel
 import com.example.mutsu.loginRegistration.AuthViewModelFactory
 import com.example.mutsu.loginRegistration.LoginRegisterScreen
+import com.example.mutsu.loginRegistration.Users
 import com.example.mutsu.navigation.AboutUs
 import com.example.mutsu.navigation.GroceryList
 import com.example.mutsu.navigation.Home
@@ -54,9 +62,7 @@ private const val INGREDIENTS_NAME_FILE = "ingredients_name"
 private const val RECIPES_FILE = "stored_recipes"
 private val dataStores = DataStoreSingleton()
 
-
 class MainActivity : ComponentActivity() {
-
     private val ingredientsNameStore : DataStore<IngredientsName> by lazy {
         dataStores.getIngredientsNameStore(this)
     }
@@ -65,152 +71,302 @@ class MainActivity : ComponentActivity() {
         dataStores.getRecipesStore(this)
     }
 
-
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MutsuApp()
+            val windowSizeClass = calculateWindowSizeClass(this)
+            MutsuApp(windowSizeClass)
         }
     }
 
-    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class,
-        ExperimentalMaterial3Api::class)
     @Composable
-    fun MutsuApp(){
-        MutsuTheme {
+    fun MutsuApp(windowSize: WindowSizeClass){
+        val navController = rememberNavController()
+
+        val authViewModel: AuthViewModel = viewModel(factory = AuthViewModelFactory())
+        val currentUser = authViewModel.currentUser().collectAsState()
+
+        MutsuTheme{
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                val navController = rememberNavController()
-
-                val currentBackStack by navController.currentBackStackEntryAsState()
-                val currentDestination = currentBackStack?.destination
-
-                val windowSizeClass = calculateWindowSizeClass(this)
-
-                val recipeViewModel = RecipeViewModel(recipesStore, this)
-                val ingredientsViewModel = IngredientsViewModel(ingredientsNameStore, this)
-
-                val authViewModel : AuthViewModel = viewModel(factory= AuthViewModelFactory())
-                var currentUser = authViewModel.currentUser().collectAsState()
-
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = {
-                                Row(verticalAlignment = Alignment.CenterVertically) {
-                                    Image(
-                                        painter = painterResource(R.drawable.applecore), // Replace with your image resource
-                                        contentDescription = "Apple core icon",
-                                        modifier = Modifier.size(50.dp) // Adjust the size as needed
-                                    )
-                                    Spacer(modifier = Modifier.width(10.dp)) // Add spacing between icon and title
-                                    Text(
-                                        text = "Mutsu",
-                                        style = MaterialTheme.typography.titleLarge,
-                                        fontWeight = FontWeight.Bold
-                                    )
-                                }
-                            }
+                when (windowSize.widthSizeClass){
+                    WindowWidthSizeClass.Compact -> {
+                        MutsuAppPortrait(
+                            navController,
+                            currentUser,
+                            authViewModel
                         )
-                    },
-                    bottomBar = {
-                        BottomAppBar(
-                            containerColor = MaterialTheme.colorScheme.primary
-                        ) {
-                            Row(
-                                horizontalArrangement = Arrangement.SpaceEvenly,
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
-                                IconButton(onClick = { navController.navigateSingleTopTo(Home.route) }){
-                                    Icon(Home.icon, contentDescription = "Home")
-                                }
-                                if (currentUser.value != null){
-                                    IconButton(
-                                        onClick = { navController.navigateSingleTopTo(Recipes.route) }
-                                    ) {
-                                        Icon(Recipes.icon, contentDescription = "Recipes")
-                                    }
-
-                                    IconButton(
-                                        onClick = { navController.navigateSingleTopTo(MealPlan.route) }
-                                    ){
-                                        Icon(MealPlan.icon, contentDescription = "Meal Plan")
-                                    }
-
-                                    IconButton(
-                                        onClick = { navController.navigateSingleTopTo(GroceryList.route) }
-                                    ){
-                                        Icon(GroceryList.icon, contentDescription = "Grocery List")
-                                    }
-                                }
-
-                                IconButton(
-                                    onClick = { navController.navigateSingleTopTo(LoginRegister.route) }
-                                ) {
-                                    Icon(LoginRegister.icon, contentDescription = "Login/Register")
-                                }
-                            }
-                        }
                     }
-                ){innerPadding ->
-                    NavHost(
-                        navController = navController,
-                        startDestination = Home.route,
-                        modifier = Modifier.padding(innerPadding)
-                    ){
-                        composable(route = Home.route){
-                            MutsuHomeScreen(
-                                goToAboutUs = {
-                                    navController.navigateSingleTopTo(AboutUs.route)
-                                }
-                            )
-                        }
-                        composable(route = MealPlan.route){
-                            foodCounter()
-                        }
-                        composable(route = Recipes.route){
-                            val newRecipeModel = RecipeViewModel(recipesStore, this@MainActivity)
-                            RecipeListScreen(
-                                goToRecipeInformation = { recipeName ->
-                                    navController.navigateToRecipeInformation(recipeName)
-                                },
-                                recipeViewModel = newRecipeModel
-                            )
-                        }
-                        composable(route = GroceryList.route){
-                            IngredientsScreen(ingredientsViewModel)
-                        }
-                        composable(route = AboutUs.route){
-                            AboutUsScreen()
-                        }
-                        composable(
-                            route = RecipeInformation.routeWithArgs,
-                            arguments = RecipeInformation.arguments
-                        ) { navBackStackEntry ->
-                            val recipeName =
-                                navBackStackEntry.arguments?.getString(RecipeInformation.recipeNameArg)
-
-                            RecipeInformationScreen(
-                                recipeViewModel = recipeViewModel,
-                                recipeName = recipeName,
-                                goToRecipeList = { navController.navigateSingleTopTo(Recipes.route) }
-                            )
-                        }
-                        composable(route = LoginRegister.route){
-                            LoginRegisterScreen(authViewModel)
-                        }
+                    else -> {
+                        MutsuAppLandscape(
+                            navController,
+                            currentUser,
+                            authViewModel
+                        )
                     }
                 }
             }
         }
     }
+
+    @Composable
+    fun MutsuAppLandscape(
+        navController: NavHostController,
+        currentUser: State<Users?>,
+        authViewModel: AuthViewModel
+    ) {
+        Row {
+            MutsuNavigationRail(
+                modifier = Modifier,
+                navController = navController,
+                currentUser = currentUser
+            )
+            HomeScreen(
+                modifier = Modifier,
+                navController = navController,
+                authViewModel = authViewModel
+            )
+        }
+    }
+
+    @Composable
+    fun MutsuAppPortrait(
+        navController: NavHostController,
+        currentUser: State<Users?>,
+        authViewModel: AuthViewModel
+    ) {
+        Scaffold(
+            topBar = { MutsuTopAppBar() },
+            bottomBar = {
+                MutsuBottomNavigation(
+                    navController = navController,
+                    currentUser = currentUser
+                )
+            }
+        ) {innerPadding ->
+            HomeScreen(
+                navController,
+                authViewModel,
+                Modifier.padding(innerPadding)
+            )
+        }
+    }
+
+    @Composable
+    fun HomeScreen(
+        navController : NavHostController,
+        authViewModel : AuthViewModel,
+        modifier: Modifier = Modifier
+    ) {
+        val recipeViewModel = RecipeViewModel(recipesStore, this)
+        val ingredientsViewModel = IngredientsViewModel(ingredientsNameStore, this)
+
+        NavHost(
+            navController = navController,
+            startDestination = Home.route,
+            modifier = modifier
+        ) {
+            composable(route = Home.route) {
+                MutsuHomeScreen(
+                    goToAboutUs = {
+                        navController.navigateSingleTopTo(AboutUs.route)
+                    }
+                )
+            }
+            composable(route = MealPlan.route) {
+                foodCounter()
+            }
+            composable(route = Recipes.route) {
+                val newRecipeModel = RecipeViewModel(recipesStore, this@MainActivity)
+                RecipeListScreen(
+                    goToRecipeInformation = { recipeName ->
+                        navController.navigateToRecipeInformation(recipeName)
+                    },
+                    recipeViewModel = newRecipeModel
+                )
+            }
+            composable(route = GroceryList.route) {
+                IngredientsScreen(ingredientsViewModel)
+            }
+            composable(route = AboutUs.route) {
+                AboutUsScreen()
+            }
+            composable(
+                route = RecipeInformation.routeWithArgs,
+                arguments = RecipeInformation.arguments
+            ) { navBackStackEntry ->
+                val recipeName =
+                    navBackStackEntry.arguments?.getString(RecipeInformation.recipeNameArg)
+
+                RecipeInformationScreen(
+                    recipeViewModel = recipeViewModel,
+                    recipeName = recipeName,
+                    goToRecipeList = { navController.navigateSingleTopTo(Recipes.route) }
+                )
+            }
+            composable(route = LoginRegister.route) {
+                LoginRegisterScreen(authViewModel)
+            }
+        }
+    }
+
+    @Composable
+    private fun MutsuBottomNavigation(
+        navController : NavHostController,
+        currentUser: State<Users?>
+    ) {
+        BottomAppBar(
+            containerColor = MaterialTheme.colorScheme.primary
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                IconButton(
+                    onClick = { navController.navigateSingleTopTo(Home.route) }
+                ) {
+                    Icon(Home.icon, contentDescription = "Home")
+                }
+
+                if (currentUser.value != null) {
+                    IconButton(
+                        onClick = { navController.navigateSingleTopTo(Recipes.route) }
+                    ) {
+                        Icon(Recipes.icon, contentDescription = "Recipes")
+                    }
+
+                    IconButton(
+                        onClick = { navController.navigateSingleTopTo(MealPlan.route) }
+                    ) {
+                        Icon(MealPlan.icon, contentDescription = "Meal Plan")
+                    }
+
+                    IconButton(
+                        onClick = { navController.navigateSingleTopTo(GroceryList.route) }
+                    ) {
+                        Icon(GroceryList.icon, contentDescription = "Grocery List")
+                    }
+                }
+
+                IconButton(onClick = { navController.navigateSingleTopTo(LoginRegister.route) }) {
+                    Icon(LoginRegister.icon, contentDescription = "Login/Register")
+                }
+            }
+        }
+    }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MutsuTopAppBar(){
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(R.drawable.applecore),
+                    contentDescription = "Apple core icon",
+                    modifier = Modifier.size(50.dp)
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(
+                    text = "Mutsu",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    )
 }
 
-fun NavHostController.navigateSingleTopTo(route: String) =
+@Composable
+fun MutsuNavigationRail(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    currentUser: State<Users?>
+) {
+    NavigationRail(
+        modifier = Modifier,
+        containerColor = MaterialTheme.colorScheme.primary,
+    ) {
+        Column(
+            modifier = modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Home.icon,
+                        contentDescription = "Home"
+                    )
+                },
+                selected = false,
+                onClick = { navController.navigateSingleTopTo(Home.route) }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            if (currentUser.value != null) {
+                NavigationRailItem(
+                    icon = {
+                        Icon(
+                            imageVector = Recipes.icon,
+                            contentDescription = "Recipe Collection"
+                        )
+                    },
+                    selected = false,
+                    onClick = { navController.navigateSingleTopTo(Recipes.route) }
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                NavigationRailItem(
+                    icon = {
+                        Icon(
+                            imageVector = MealPlan.icon,
+                            contentDescription = "Meal Plan"
+                        )
+                    },
+                    selected = false,
+                    onClick = { navController.navigateSingleTopTo(MealPlan.route) }
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                NavigationRailItem(
+                    icon = {
+                        Icon(
+                            imageVector = GroceryList.icon,
+                            contentDescription = "Grocery List"
+                        )
+                    },
+                    selected = false,
+                    onClick = { navController.navigateSingleTopTo(GroceryList.route) }
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = LoginRegister.icon,
+                        contentDescription = "Log in and Register"
+                    )
+                },
+                selected = false,
+                onClick = { navController.navigateSingleTopTo(LoginRegister.route) }
+            )
+        }
+    }
+}
+
+private fun NavHostController.navigateSingleTopTo(route: String) {
     this.navigate(route) { launchSingleTop = true }
+}
 
 private fun NavHostController.navigateToRecipeInformation(recipeName: String) {
-    this.navigateSingleTopTo("${RecipeInformation.route}/$recipeName")
+    this.navigateSingleTopTo("${RecipeInformation.route}/$recipeName")}
 }
